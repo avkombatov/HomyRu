cbdeda71ee5bfdd529eccef2195052a49497e23a
kombat <avkombatov@yandex.ru>
2021-09-10 22:49:28 +0300

    начат блок акции

--- js/products.js
+++ js/products.js
@@ -1,21 +1,24 @@
 let offset = 0; // смещение
+// const buttonPrev = document.querySelectorAll('.product__prev');
+// const buttonNext = document.querySelectorAll('.product__next');
+// const slider = document.querySelectorAll('.products__box');
+
 const buttonPrev = document.querySelector('.product__prev');
 const buttonNext = document.querySelector('.product__next');
 const slider = document.querySelector('.products__box');
+buttonNext.addEventListener('click', function () {
+    offset = offset - 275;
+    if (offset < 0) {
+        offset = 1100;
+    }
+    slider.style.left = -offset + 'px';
 
+});
 
 buttonPrev.addEventListener('click', function () {
     offset = offset + 275;
     if (offset > 1100) {
         offset = 0;
-    }
-    slider.style.left = -offset + 'px';
-
-});
-buttonNext.addEventListener('click', function () {
-    offset = offset - 275;
-    if (offset < 0) {
-        offset = 1100;
     }
     slider.style.left = -offset + 'px';
 
